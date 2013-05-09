d8ab1a16471ffa865581d7d627ed9d89f4d616c6
erik <wangjingbupt@gmail.com>
2013-05-09 14:00:11 +0800

    about

--- view/about.php
+++ view/about.php
@@ -10,29 +10,28 @@ class ViewAbout {
 		$html =<<<HTML
 		<div class="container"><div class="row-fluid"><div class = "span12">
 		<br/>
+		<blockquote>
+			<p class="lead">All the moments will be lost in time,like tears in rain</p>
+			<small><cite title="Source Title">Blade Runner</cite></small>
+		</blockquote>
+		<br/>
 		<br/>
 		<p>学习、工作、生活在北京。混迹在互联网，干着技术活。ps.不会修电脑！</p>
-		<p class="text-info">work@Baidu Music</p>
-		<p class="text-success">worked@Ganji</p>
-		<p class="text-success">worked@Sina Blog</p>
-		<p class="muted">PHP\Python\Shell\awk</p>
-		<p class="muted">Mysql\Redis\Memcached\Mongodb</p>
-		<p class="muted">Nginx\Apache\Lighttp\Squid</p>
+		<p >PHP\Python\Shell\awk</p>
+		<p >Mysql\Redis\Memcached\Mongodb</p>
+		<p >Nginx\Apache\Lighttp\Squid</p>
 		<p>最近喜欢上NLP和Network</p>
 		<br/>
-		<p>北京邮电大学 遵义航天高级中学</p>
-		<p>Born@遵义</p>
+		<p class="text-info">Work@百度音乐</p>
 		<p>Living@北京</p>
+		<p>Born@遵义</p>
+		<p>北京邮电大学 遵义航天高级中学</p>
+		<p>曾在赶集、新浪博客搬砖</p>
+		<br/>
 		<p>Email: wangjingbupt@gmail.com</p>
 		<p>Weibo: <a href='http://weibo.com/811161987'>@京京道听途说</a></p>
 		<br/>
 		<p>业余跑步爱好者，eason fans，尤文死忠，爱音乐、爱运动。</p>
-		<br/>
-		<br/>
-		<blockquote>
-			<p class="lead">All the monments will be lost in time,like tears in rain</p>
-			<small>Roy Batty <cite title="Source Title">Blade Runner</cite></small>
-		</blockquote>
 		</div></div></div>
 HTML;
 		echo $html;
